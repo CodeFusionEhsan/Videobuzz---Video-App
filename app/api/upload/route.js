26f56034d296ee29ae726d85411149a8771981cd
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-01-24 23:46:15 +0530

    Update route.js

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -20,7 +20,7 @@ export async function POST(req, Response) {
       const buffer = Buffer.from(await file.arrayBuffer())
       const filename = Date.now() + file.name.replaceAll(" ", "_")
       await writeFile(path.join(process.cwd(), "public/videos/"+ filename), buffer)
-      await mongoose.connect("mongodb+srv://ehsan:ehsan2024@cluster0.vqrb8yl.mongodb.net/Videobuzz?retryWrites=true&w=majority")
+      await mongoose.connect(process.env.MONGO_URI)
       console.log("Connected to database")
 
       const newupload = new Video({
