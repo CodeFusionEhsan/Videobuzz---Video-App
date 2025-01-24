19067c82bdb90a46f5af71917a9ed5856155dd32
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-01-24 23:36:02 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,14 +11,14 @@ const handler = NextAuth({
   providers: [
     GithubProvider({
       clientId: "Ov23liu8zVVjiNFEoppC",
-      clientSecret: env.NEXTAUTH_SECRET,
+      clientSecret: "cee87be3d11a22e45a2281be7451acc8c1dc35a4",
     }),
     GoogleProvider({
         clientId: "102872295334-5jrotsdl19hitothra1tdrt9l86k4cuu.apps.googleusercontent.com",
-        clientSecret: env.NEXTAUTH_SECRET,
+        clientSecret: "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",
     }),
   ],
-  secret: env.NEXTAUTH_URL
+  secret: "73f8d9b1-e456-42cf-9a32-af72bfc6c7e0_XzJyb3p6YnVtcF8yNk5vczY3NjA3NTQjISQy"
 })
 
 export {handler as GET, handler as POST}
